614d11c5077b40442a67d9509a2576bbd46af5dc
Alex <alex@hagl.de>
2023-12-28 16:42:56 +0100

    Baby Shark lel

---
+++ Sharc.Core/Entities/Event.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Sharc.Core.Entities; 
+
+public class Event {
+    
+    public Guid Id { get; set; }
+    // CN
+    public string Name { get; set; }
+    // DESCRIPTION
+    public string Description { get; set; }
+    // SUMMARY
+    public string Summary { get; set; }
+    // DTSTAMP
+    public DateTime Created { get; set; }
+    // DTEND
+    public DateTime EndTime { get; set; }
+    // DTSTART
+    public DateTime StartTime { get; set; }
+
+    // ATTENDEE
+    public User[] Attendees { get; set; }
+
+
+    [NotMapped]
+    public TimeSpan Duration {
+        get => EndTime.Subtract(StartTime);
+        set => EndTime = StartTime.Add(value);
+    }
+
+    public override string ToString() {
+        return $"""
+                BEGIN:VEVENT
+                UID:{DateTimeToRFCTime(Created)}-{Id}
+                CN:{Name}
+                DTSTAMP:{DateTimeToRFCTime(Created)}
+                DTSTART:{DateTimeToRFCTime(StartTime)}
+                DTEND:{DateTimeToRFCTime(EndTime)}
+                SUMMARY:{Summary}
+                DESCRIPTION:{Description}
+                END:VEVENT
+                """;
+    }
+
+    private static string DateTimeToRFCTime(DateTime time) {
+        return time.ToUniversalTime().ToString("yyyyMMddTHHmmss");
+    }
+}
---
+++ Sharc.Core/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Sharc.Core.Entities; 
+
+public class User {
+    
+    public Guid Id { get; set; }
+    
+    public string IdentityId { get; set; }
+    
+    public string CachedUsername { get; set; }
+}

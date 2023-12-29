b94543549b405ca2d8cbf8ddbc9d0f0981a298d2
Alex <alex@hagl.de>
2023-12-29 17:25:05 +0100

    configured entities with fluent api

---
+++ Sharc.Server.Data/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Sharc.Core.Entities;
+
+namespace Sharc.Server.Data.Persistence; 
+
+public class ApplicationDbContext : DbContext {
+    
+    public DbSet<Event> Events { get; set; }
+    public DbSet<User> Users { get; set; }
+
+    public ApplicationDbContext(DbContextOptions opt) : base(opt) {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        modelBuilder.Entity<Event>()
+            .HasKey(e => e.Id);
+        modelBuilder.Entity<User>()
+            .HasKey(e => e.Id);
+        modelBuilder.Entity<Event>()
+            .HasMany<User>(e => e.Attendees)
+            .WithMany();
+        
+        ConfigureEventProperties(modelBuilder);
+        ConfigureUserProperties(modelBuilder);
+    }
+
+    private void ConfigureUserProperties(ModelBuilder modelBuilder) {
+        modelBuilder.Entity<User>()
+            .Property(e => e.CachedUsername)
+            .IsRequired()
+            .HasMaxLength(36);
+
+        modelBuilder.Entity<User>()
+            .Property(e => e.Email)
+            .IsRequired();
+
+        modelBuilder.Entity<User>()
+            .Property(e => e.IdentityId)
+            .IsRequired();
+    }
+
+    private void ConfigureRecurrenceRuleProperties(OwnedNavigationBuilder<Event, RecurrenceRule> builder) {
+        builder
+            .Property(e => e.Frequency)
+            .IsRequired();
+    }
+    
+    private void ConfigureEventProperties(ModelBuilder modelBuilder) {
+        modelBuilder.Entity<Event>()
+            .OwnsOne<RecurrenceRule>(e => e.RecurrenceRule, ConfigureRecurrenceRuleProperties );
+
+        
+        modelBuilder.Entity<Event>()
+            .Property(e => e.Created)
+            .ValueGeneratedOnAddOrUpdate();
+
+        modelBuilder.Entity<Event>()
+            .Property(e => e.Description)
+            .HasMaxLength(255);
+
+        modelBuilder.Entity<Event>()
+            .Property(e => e.Summary)
+            .IsRequired()
+            .HasMaxLength(128);
+
+        modelBuilder.Entity<Event>()
+            .Property(e => e.EndTime)
+            .IsRequired();
+        modelBuilder.Entity<Event>()
+            .Property(e => e.StartTime)
+            .IsRequired();
+    }
+}
